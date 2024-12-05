e00e811dd9e78b2f0905ddd17f932957f8196fe3
ZykeLadebrouile <gemicieneshakki@gmail.com>
2024-12-05 21:12:22 +0100

    Add shebang and docstrings to Python scripts for type annotations

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,3 +1,8 @@
+#!/usr/bin/env python3
+"""a script for type-annotated function add that
+takes a float a and a float b as arguments
+and returns their sum as a float."""
+
 def add(a: float, b: float) -> float:
     """
     Adds two float numbers and returns the result.
